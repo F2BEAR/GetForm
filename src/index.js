239def7f134c3ae61efa86995d3a13ca6f49a144
Facundo <facucarbonel@hotmail.com>
2020-01-17 16:05:27 -0300

    Separation of concerns and functional Login
    Well, first of all, I'd separate the app in 3 different html's. 
    
    The first on is home (index.html), which is the entry point of the app, it just has the header and the text that the last version has and a navbar with for navigation trough pages. 
    Sign in, it's the same form that we had before but now it has it's own new and fancy page. 
    And for last but not least, the login page, where you put the user's email and password. Then Node asks MongoDB for the information of that email account, it  hash and salt the input password using the same salt of the saved password, compares the saved password and the inputs password. And if it's all correct, the user is sent back to index and on the console you'll see the message "LogIn made successfully!".

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const path = require("path");
-
 const MongoClient = require("mongodb").MongoClient;
-
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
 
 const express = require("express");
-
 const app = express();
 
 require("dotenv").config();
@@ -28,17 +26,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  res.statusCode = 200;
 });
 
 app.get("/styles.css", (req, res) => {
   res.sendFile(path.join(__dirname, "styles.css"));
+  res.statusCode = 200;
 });
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+  res.statusCode = 200;
+});
+
+app.get("/signin", (req, res) => {
+  res.sendFile(path.join(__dirname, "signin.html"));
+  res.statusCode = 200;
+});
+
 app.post("/new", (req, res) => {
+  const password = req.body.usrPassword;
+
+  const hashPassword = password => {
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto
+      .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+      .toString(`hex`);
+
+    var hash = crypto
+      .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+      .toString(`hex`);
+
+    return hash;
+  };
+
+  let hashedPassword = hashPassword(password);
+
   let myObj = {
     name: req.body.usrName,
     email: req.body.usrMail,
     age: req.body.usrAge,
-    password: req.body.usrPassword
+    password: hashedPassword,
+    salt: this.salt
   };
   client.connect((err, db) => {
     if (err) {
@@ -51,11 +80,63 @@ app.post("/new", (req, res) => {
     collection.insertOne(myObj, function(err, r) {
       if (err) {
         console.log("cannot add obj");
+        res.statusMessage = "Insert Error";
+        res.statusCode = 400;
         return;
       }
 
       console.log("Added a user");
       res.redirect("/");
+      res.statusCode = 200;
+    });
+    client.close();
+  });
+});
+
+app.post("/log", (req, res) => {
+  client.connect((err, db) => {
+    if (err) {
+      console.log("cannot connect db" + err);
+      res.statusMessage = "Insert Error";
+      res.statusCode = 400;
+      return;
+    }
+    console.log("DataBase connection made successfully");
+
+    const collection = db.db().collection("UserData");
+
+    const mail = req.body.usrMail;
+    const password = req.body.usrPassword;
+
+    collection.findOne({ email: mail }, (err, doc) => {
+      if (err) {
+        console.log("cannot make query" + err);
+        res.statusMessage = "Insert Error";
+        res.statusCode = 400;
+        return;
+      }
+      let savedPass = doc.password;
+      let savedSalt = doc.salt;
+
+      const hashPassword = password => {
+        this.hash = crypto
+          .pbkdf2Sync(password, savedSalt, 1000, 64, `sha512`)
+          .toString(`hex`);
+
+        var hash = crypto
+          .pbkdf2Sync(password, savedSalt, 1000, 64, `sha512`)
+          .toString(`hex`);
+
+        return hash;
+      };
+
+      let hashedPassword = hashPassword(password);
+
+      if (hashedPassword === savedPass) {
+        console.log("LogIn made successfully!");
+        res.redirect("/");
+        res.statusCode = 200;
+      }
     });
     client.close();
   });
